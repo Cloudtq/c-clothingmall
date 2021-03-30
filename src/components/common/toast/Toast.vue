3b5aec8261c672be1f89a0699e6094e1d63c20c7
LAPTOP-HPL6MM6C\czd <1148026508@qq.com>
2021-03-30 21:23:43 +0800

    使用fastclick、lazyload进行优化，新增toast弹窗

---
+++ src/components/common/toast/Toast.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="toast" v-show="isShow">
+      {{message}}
+  </div>
+</template>
+<script>
+export default { 
+   name: "Toast",
+   data(){
+       return {
+           message:'',
+           isShow:false
+       }
+   },
+   methods:{
+       show(message='默认文字',duration=2000){
+            this.isShow = true
+            this.message = message
+            setTimeout(()=>{
+                this.isShow = false
+           },duration)
+       }
+   }
+} 
+</script>
+<style scoped>
+
+    .toast{
+        transform: translate(-50%,-50%);
+        color: #fff;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        background-color: rgba(0, 0, 0, .7);
+        padding: 8px 10px;
+        z-index: 999;
+    }
+</style>
